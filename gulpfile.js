4f5ce3509e0d058c02efc5783fda0705ba951748
Stephen Poole <stephen.poole@redlioncanada.com>
2016-11-30 13:42:37 -0500

    Added assets.

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var gulp = require('gulp'),
 
 gulp.task('css', function() {
 	gulp.src('./app/css/index.scss')
-		.pipe(sass())
+		.pipe(sass()).on('error', handleError)
 		.pipe(minifyCss())
 		.pipe(gulp.dest('./app/css'))
 })
@@ -16,13 +16,24 @@ gulp.task('html', function() {
 	gulp.src('./app/index.html')
 		.pipe(include())
 		.pipe(inliner())
-		.pipe(minifyHtml())
+		.pipe(minifyHtml()).on('error', handleError)
 		.pipe(gulp.dest('./build'))
 
 })
 
-gulp.task('once', ['html', 'css'], function() {})
+gulp.task('assets', function() {
+	gulp.src('./app/assets/**/*')
+		.pipe(gulp.dest('./build')).on('error', handleError)
+})
+
+gulp.task('once', ['html', 'css', 'assets'], function() {})
 
 gulp.task('default', ['once'], function() {
-	gulp.watch('./app/**/*', ['html', 'css'])
+	gulp.watch('./app/css/**/*.scss', ['html', 'css'])
+	gulp.watch('./app/index.html', ['html', 'css'])
+	gulp.watch('./app/assets/**/*', ['assets'])
 })
+
+function handleError(err) {
+	console.error(err.message || err.msg || err)
+}
